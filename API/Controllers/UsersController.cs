ab4c7f051217a678b02e001d390ba2f316f6c673
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-11-13 23:55:04 +0100

    feat: Endpoint to update user info
    Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]  
+    [Authorize]   
     public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
@@ -37,6 +38,21 @@ namespace API.Controllers
         {
             return await _userRepository.GetMemberAsync(username);
 
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
