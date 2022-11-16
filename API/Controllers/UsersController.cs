879c6e6a6c09a2b79d5a31e0581fd0ccd181767b
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-11-16 18:58:33 +0100

    Api response fixed

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -37,7 +37,7 @@ namespace API.Controllers
         }
 
         // Returns the user associated with the provided ID
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -81,7 +81,9 @@ namespace API.Controllers
             user.Photos.Add(photo);
 
             if (await _userRepository.SaveAllAsync())
-                return _mapper.Map<PhotoDto>(photo);
+            {
+                return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+            }
 
             return BadRequest("Problem addind photo");
         }
