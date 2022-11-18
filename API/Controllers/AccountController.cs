41427aa5ed045e74370ad3b8120486ec4bbf3fd0
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-11-18 23:20:16 +0100

    Refactor: Login method returns main photo

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -65,7 +66,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
