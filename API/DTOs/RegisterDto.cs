44c3d66aba2af9a89bec1fe7bd86b3a5af69f91c
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-11-19 00:15:19 +0100

    RegisterDto expanded

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 
@@ -5,11 +6,15 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required]
-        public string Username { get; set; }
+        [Required] public string Username { get; set; }
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public DateTime DateOfBirth { get; set; }
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
 
         [Required]
         [StringLength(8, MinimumLength = 4)]
-        public string Password { get; set; }
+        public string Password { get; set; } 
     }
 }
