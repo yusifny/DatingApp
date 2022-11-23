8b119411d8aa706bac25e885120723f2bcc550dc
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-11-23 22:57:23 +0100

    ClaimPrinciplesExtension updated

--- API/Extensions/ClaimsPrincipleExtension.cs
+++ API/Extensions/ClaimsPrincipleExtension.cs
@@ -1,7 +1,4 @@
 using System.Security.Claims;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.Extensions
 {
@@ -9,7 +6,12 @@ namespace API.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }        
+        
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
         }
     }
 }
