329dfeb3a7469b385d6d92f86bca666af13f2bad
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-12-01 22:28:18 +0100

    Feat: Implemented getting messages

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -28,11 +30,11 @@ namespace API.Controllers
         {
             var username = User.GetUsername();
 
-            if(username == createMessageDto.RecipientUserName.ToLower())
+            if(username == createMessageDto.RecipientUsername.ToLower())
                 return BadRequest("You cannot send messages to yourself");
 
             var sender = await _userRepository.GetUserByUsernameAsync(username);
-            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUserName);
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
 
             if(recipient == null) return NotFound();
 
@@ -41,14 +43,29 @@ namespace API.Controllers
                 Sender = sender,
                 Recipient = recipient,
                 SenderUsername = sender.UserName,
-                RecipientUsername = recipient.UserName
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
             };
 
             _messageRepository.AddMessage(message);
 
-            if(await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message ));
+            if(await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
 
             return BadRequest("Failed to send message"); 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] 
+            MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
+
+            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, 
+                messages.TotalCount, messages.TotalPages);
+
+            return messages;
         }
     }
 }
