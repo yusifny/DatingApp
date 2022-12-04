33fee7c5073eae2154faf35fc415b4152355fd7f
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-12-05 00:15:30 +0100

    refactor: :fire: removes manual cryptography

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -30,12 +28,8 @@ namespace API.Controllers
             if(await UserExist(registerDto.Username)) return BadRequest("Username is taken");
 
             var user = _mapper.Map<AppUser>(registerDto);
-            
-            using var hmac = new HMACSHA512();
 
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -56,15 +50,6 @@ namespace API.Controllers
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if(user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for(int i = 0; i < computeHash.Length; i++)
-            {
-                if(computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-            }
 
             return new UserDto
             {
--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
@@ -19,11 +17,7 @@ namespace API.Data
 
             foreach(var user in users)
             {
-                using var hmac = new HMACSHA512();
- 
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
-                user.PasswordSalt = hmac.Key;
 
                 context.Users.Add(user);
             }
