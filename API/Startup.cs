ac1788b4cbc7b0a93c861f94a083491c24033897
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-12-14 00:48:13 +0100

    feat: uses auth in signalr

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,7 +48,10 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
             
