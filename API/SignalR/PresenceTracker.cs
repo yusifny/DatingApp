552c635a573b3e767ff70f0a2ef12d93a7d1327c
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-12-15 02:04:21 +0100

    refactor: optimizes presence

--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -9,8 +9,10 @@ namespace API.SignalR
         private static readonly Dictionary<string, List<string>> OnlineUsers = 
             new Dictionary<string, List<string>>();
 
-        public Task UserConnnected(string username, string connectionId)
+        public Task<bool> UserConnnected(string username, string connectionId)
         {
+            bool isOnline = false;
+
             lock (OnlineUsers)
             {
                 if(OnlineUsers.ContainsKey(username))
@@ -20,26 +22,30 @@ namespace API.SignalR
                 else
                 {
                     OnlineUsers.Add(username, new List<string>{connectionId});
+                    isOnline = true;
                 }
             }
 
-            return Task.CompletedTask;
+            return Task.FromResult(isOnline);
         }
 
-        public Task UserDisconnected(string username, string connectionId)
+        public Task<bool> UserDisconnected(string username, string connectionId)
         {
+            bool isOffline = false;
+
             lock (OnlineUsers)
             {
-                if(!OnlineUsers.ContainsKey(username)) return  Task.CompletedTask;
+                if(!OnlineUsers.ContainsKey(username)) return  Task.FromResult (isOffline);
 
                 OnlineUsers[username].Remove(connectionId);
                 if(OnlineUsers[username].Count == 0)
                 {
                     OnlineUsers.Remove(username);
+                    isOffline = true;
                 }
             }
 
-            return Task.CompletedTask;
+            return Task.FromResult(isOffline);
         }
 
         public Task<string[]> GetOnlineUsers()
